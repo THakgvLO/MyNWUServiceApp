ec579b4478013e3e03359ae723b39de183efde7b
Thakgalo Sehlola <132753783+THakgvLO@users.noreply.github.com>
2024-09-23 12:59:44 +0200

    uploading my MainActivity.java and activity_main.xml

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.myapplication;
+
+import android.os.*;
+import android.view.View;
+import android.net.Uri;
+import android.widget.Button;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void homeButton(View h)
+    {
+        int button3 = R.id.NWU;
+        Button openWebsiteButton = findViewById(button3);
+        openWebsiteButton.setOnClickListener(v -> {
+            // Create an Intent to open the website
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.nwu.ac.za/"));
+            startActivity(browserIntent);
+        });
+    }
+
+    public void rateService(View r)
+    {
+        Button openRateServiceButton = findViewById(R.id.rateButton);
+        openRateServiceButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RateServiceActivity.class);
+            startActivity(intent);
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}*/
